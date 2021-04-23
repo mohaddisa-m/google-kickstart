4334181058acf557f884874dd918ea66a39341f0
Mohaddisa Maryam <mohaddisa321@gmail.com>
2021-04-23 17:53:20 +0500

    kickstart

--- a.cc
+++ a.cc
@@ -1,29 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int x = 0;
-    cin >> x;
-    for (int i = 1; i <= x; i++)
-    {
-        cout << "Case #" << i << ": ";
-        int z = 0;
-        cin >> z;
-
-        string s ;
+    int t;cin >>t;
+    for (int case_num = 1; case_num<=t; case_num ++){
+        int N , K; 
+        cin >>N>>K;
+        string s;
         cin >> s;
-        int ans = 0;
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (s[i]>s[i-1])
-            {
-                ans++;
-            } else {
-                ans = 1;
-            }
-            cout<< ans << " ";
-        }
-        cout << "\n";
+    int cur_score = 0;
+    for (int i = 0; i < N/2; i++)
+    {
+        cur_score += (s[i]!=s[N-1-i]);
     }
+    cout << "Case #" << case_num <<": " << abs(cur_score-K)<< endl;   
+    }
+    return 0;
 }
